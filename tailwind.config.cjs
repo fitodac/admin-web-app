a0e5e754045c694317e76f747125c5c59f18fd98
fitodac <fitodac@gmail.com>
2023-03-24 07:36:02 -0300

    initial commit

---
+++ tailwind.config.cjs
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+	content: [
+		'./src/**/*.{js,jsx,ts,tsx}',
+		'./src/routes/**/*.{js,jsx,ts,tsx}',
+		'index.html'
+	],
+
+	theme: {
+		extend: {},
+	},
+
+	plugins: []
+}
